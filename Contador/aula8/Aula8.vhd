896fa158c66fbbbf385ea9f310bcb29b1716844b
keiyanishio <keiyan@al.insper.edu.br>
2023-04-19 09:07:51 -0300

    aula 8 parte 2 acho q ta feito

--- Contador/aula8/Aula8.vhd
+++ Contador/aula8/Aula8.vhd
@@ -12,7 +12,8 @@ entity Aula8 is
     KEY: in std_logic_vector(3 downto 0);
 	 SW : in std_logic_vector (9 downto 0);
 	 LEDR: out std_logic_vector (9 downto 0);
-	 HEX0, HEX1, HEX2, HEX3, HEX4, HEX5: out std_logic_vector (6 downto 0)
+	 HEX0, HEX1, HEX2, HEX3, HEX4, HEX5: out std_logic_vector (6 downto 0);
+	 FPGA_RESET: in std_logic
   );
 end entity;
 
@@ -44,6 +45,16 @@ architecture arquitetura of Aula8 is
   signal dis3 : std_logic_vector (6 downto 0);
   signal dis4 : std_logic_vector (6 downto 0);
   signal dis5 : std_logic_vector (6 downto 0);
+  
+  signal hab_sw0a7 : std_logic;
+  signal hab_sw8 : std_logic;
+  signal hab_sw9 : std_logic;
+  
+  signal hab_key0 : std_logic;
+  signal hab_key1 : std_logic;
+  signal hab_key2 : std_logic;
+  signal hab_key3 : std_logic;
+  signal hab_reset : std_logic;
   
 begin
 
@@ -90,7 +101,39 @@ display : entity work.display
              port map (CLK => CLK, data_out => saida_reg (3 downto 0), wr => wr, bloco_4 => saida_dec1(4),
 				          data_adr_5 => data_adr(5), endereco => saida_dec2 (5 downto 0), HEX0 => dis0,
 							 HEX1 => dis1, HEX2=>dis2, HEX3 => dis3, HEX4 => dis4, HEX5 => dis5);
+							 
 
+							 
+							 
+sw0a7: entity work.buffer_3_state_8portas
+            port map (entrada => SW (7 downto 0), habilita => hab_sw0a7, saida => saida_RAM);
+				
+sw8: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & SW(8), habilita => hab_sw8, saida => saida_RAM);
+
+sw9: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & SW(9), habilita => hab_sw9, saida => saida_RAM);
+				
+				
+				
+key0: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & KEY(0), habilita => hab_key0, saida => saida_RAM);
+				
+key1: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & KEY(1), habilita => hab_key1, saida => saida_RAM);		
+		
+key2: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & KEY(2), habilita => hab_key2, saida => saida_RAM);	
+		
+key3: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & KEY(3), habilita => hab_key3, saida => saida_RAM);	
+		
+key_reset: entity work.buffer_3_state_8portas
+            port map (entrada => "0000000" & FPGA_RESET, habilita => hab_reset, saida => saida_RAM);	
+		
+
+
+		
 -- I/O
 --chavesY_MUX_A <= SW(3 downto 0);
 --chavesX_ULA_B <= SW(9 downto 6);
@@ -113,6 +156,19 @@ HEX2 <= dis2;
 HEX3 <= dis3;
 HEX4 <= dis4;
 HEX5 <= dis5;
+
+
+
+
+hab_sw0a7<=(rd and not(data_adr(5)) and saida_dec2(0) and saida_dec1(5));
+hab_sw8 <= (rd and not(data_adr(5)) and saida_dec2(1) and saida_dec1(5));
+hab_sw9 <= (rd and not(data_adr(5)) and saida_dec2(2) and saida_dec1(5));
+
+hab_key0 <= (rd and data_adr(5) and saida_dec2(0) and saida_dec1(5));
+hab_key1 <= (rd and data_adr(5) and saida_dec2(1) and saida_dec1(5));
+hab_key2 <= (rd and data_adr(5) and saida_dec2(2) and saida_dec1(5));
+hab_key3 <= (rd and data_adr(5) and saida_dec2(3) and saida_dec1(5));
+hab_reset <= (rd and data_adr(5) and saida_dec2(4) and saida_dec1(5));
 
 hab_ff1 <= (wr and saida_dec1(4) and saida_dec2(2) and not(data_adr(5)));
 hab_ff2 <= (wr and saida_dec1(4) and saida_dec2(1) and not(data_adr(5)));
