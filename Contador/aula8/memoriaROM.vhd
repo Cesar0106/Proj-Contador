769dc010727ddaffcdba51b6f0f1c54bb65d2015
keiyanishio <keiyan@al.insper.edu.br>
2023-04-20 18:05:51 -0300

    assembly quase feito

--- Contador/aula8/memoriaROM.vhd
+++ Contador/aula8/memoriaROM.vhd
@@ -36,26 +36,68 @@ architecture assincrona of memoriaROM is
   begin
       -- Palavra de Controle = SelMUX, Habilita_A, Reset_A, Operacao_ULA
       -- Inicializa os endereços:
-		  tmp(0)    := LDI   & '0' & x"00";  
-        tmp(1)    := STA   & '0' & x"00";  
-        tmp(2)    := STA   & '0' & x"02";   
-		  tmp(3)    := LDI   & '0' & x"01";   
-        tmp(4)    := STA   & '0' & x"01";  
-        tmp(5)    := NOP   & '0' & x"00";  
-        tmp(6)    := LDA   & '1' & x"60";   
-		  tmp(7)    := STA   & '1' & x"20";
-		  tmp(8)    := CEQ   & '0' & x"00";  
-        tmp(9)    := JEQ   & '0' & x"0B";  
-        tmp(10)   := JSR   & '0' & x"20";   
-		  tmp(11)   := NOP   & '0' & x"00";
-		  tmp(12)   := JMP   & '0' & x"05";  
-        tmp(32)   := STA   & '1' & x"FF";  
-        tmp(33)   := LDA   & '0' & x"02";   
-		  tmp(34)   := SOMA  & '0' & x"01";
-		  tmp(35)   := STA   & '0' & x"02";
-		  tmp(36)   := STA   & '1' & x"02";
-		  tmp(37)   := STA   & '1' & x"25";
-		  tmp(38)   := RET   & '0' & x"00";     
+		tmp(0)    := LDI   & '0' & x"00";  
+        tmp(1)    := STA   & '1' & x"20";  
+        tmp(2)    := STA   & '1' & x"21";   
+	    tmp(3)    := STA   & '1' & x"22";   
+        tmp(4)    := STA   & '1' & x"23";  
+        tmp(5)    := STA   & '1' & x"24";  
+        tmp(6)    := STA   & '1' & x"25";
+
+		tmp(7)    := STA   & '1' & x"00";
+		tmp(8)    := STA   & '1' & x"01";  
+        tmp(9)    := STA   & '1' & x"02";
+
+        tmp(10)   := STA   & '0' & x"00";   
+		tmp(11)   := STA   & '0' & x"01";
+		tmp(12)   := STA   & '0' & x"02";  
+        tmp(13)   := STA   & '0' & x"03";  
+        tmp(14)   := STA   & '0' & x"04";   
+		tmp(15)   := STA   & '0' & x"05";
+		tmp(16)   := STA   & '0' & x"06";
+
+		tmp(17)   := LDI   & '0' & x"01";
+		tmp(18)   := STA   & '0' & x"07";
+
+        tmp(19)   := LDI   & '0' & x"10";
+		tmp(20)   := STA   & '0' & x"10";
+
+        tmp(21)   := LDA   & '1' & X"61";
+        tmp(22)   := CEQ   & '0' & X"07";
+        tmp(23)   := JEQ   & '0' & X"    ";
+
+        tmp(24)   := LDA   & '1' & X"60";
+        tmp(25)   := CEQ   & '0' & X"06";
+        tmp(26)   := JEQ   & '0' & X"1C";
+
+        tmp(27)   := JSR   & '0' & X"1E";
+
+        tmp(28)   := JSR   & '0' & X"25";
+        tmp(29)   := JMP   & '0' & X"15";
+
+        tmp(30)   := STA   & '1' & X"FF";
+        tmp(31)   := LDA   & '0' & X"00";
+        tmp(32)   := SOMA  & '0' & X"07";
+        tmp(33)   := STA   & '0' & X"00";
+        tmp(34)   := CEQ   & '0' & X"0A";
+        tmp(35)   := JEQ   & '0' & X"  ";
+        tmp(36)   := RET   & '0' & X"00";
+
+
+        tmp(37)   := LDA   & '0' & X"00";
+        tmp(38)   := STA   & '1' & X"20";
+        tmp(39)   := LDA   & '0' & X"01";
+        tmp(40)   := STA   & '1' & X"21";
+        tmp(41)   := LDA   & '0' & X"02";
+        tmp(42)   := STA   & '1' & X"22";
+        tmp(43)   := LDA   & '0' & X"03";
+        tmp(44)   := STA   & '1' & X"23";
+        tmp(45)   := LDA   & '0' & X"04";
+        tmp(46)   := STA   & '1' & X"24";
+        tmp(47)   := LDA   & '0' & X"05";
+        tmp(48)   := STA   & '1' & X"25";
+        tmp(49)   := RET   & '0' & X"00";
+
 		  
         return tmp;
     end initMemory;
